cbffde4fe3dde64af9daea6fabeef6d9e03b700c
imomarksasa <41761073+imomarksasa@users.noreply.github.com>
2018-08-17 13:11:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,23 +11,6 @@ client.on('message', message => {
   	}
 });
 
-client.on('ready', function(){    
-    var ms = 40000 ;    
-    var setGame = ['MTA SA'];    
-    var i = -1;    
-    var j = 0;    
-    setInterval(function (){    
-        if( i == -1 ){    
-j = 1;    
-       }    
-        if( i == (setGame.length)-1 ){    
-            j = -1;    
-      }    
-       i = i+j;    
-        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);    
-}, ms);    
-    
-});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
